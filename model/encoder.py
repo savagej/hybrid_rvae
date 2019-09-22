722ba06aedcba44f7a1e6af46851a1c797139d4b
John Savage <dtjohnnyb@gmail.com>
2019-09-22 13:20:30 +0100

    Added option to return output from different layers of encoder

--- model/encoder.py
+++ model/encoder.py
@@ -11,51 +11,53 @@ class Encoder(nn.Module):
         self.latent_size = latent_size
 
         if max_seq_len == 50:
-            self.cnn = nn.Sequential(
-                nn.Conv1d(self.embed_size, 128, 4, 2),
+            self.layered_layers = [
+                [nn.Conv1d(self.embed_size, 128, 4, 2),
                 nn.BatchNorm1d(128),
-                nn.ELU(),
+                nn.ELU()],
 
-                nn.Conv1d(128, 256, 4, 2),
+                [nn.Conv1d(128, 256, 4, 2),
                 nn.BatchNorm1d(256),
-                nn.ELU(),
+                nn.ELU()],
 
-                nn.Conv1d(256, 512, 4, 2),
+                [nn.Conv1d(256, 512, 4, 2),
                 nn.BatchNorm1d(512),
-                nn.ELU(),
+                nn.ELU()],
 
-                nn.Conv1d(512, self.latent_size, 4, 2),
+                [nn.Conv1d(512, self.latent_size, 4, 2),
                 nn.BatchNorm1d(self.latent_size),
-                nn.ELU()
-            )
+                nn.ELU()]
+            ]
+
         elif max_seq_len == 209:
-            self.cnn = nn.Sequential(
-                nn.Conv1d(self.embed_size, 128, 4, 2),
+            self.layered_layers = [
+                [nn.Conv1d(self.embed_size, 128, 4, 2),
                 nn.BatchNorm1d(128),
-                nn.ELU(),
+                nn.ELU()],
 
-                nn.Conv1d(128, 256, 4, 2),
+                [nn.Conv1d(128, 256, 4, 2),
                 nn.BatchNorm1d(256),
-                nn.ELU(),
+                nn.ELU()],
 
-                nn.Conv1d(256, 256, 4, 2),
+                [nn.Conv1d(256, 256, 4, 2),
                 nn.BatchNorm1d(256),
-                nn.ELU(),
+                nn.ELU()],
 
-                nn.Conv1d(256, 512, 4, 2),
+                [nn.Conv1d(256, 512, 4, 2),
                 nn.BatchNorm1d(512),
-                nn.ELU(),
+                nn.ELU()],
 
-                nn.Conv1d(512, 512, 4, 2),
+                [nn.Conv1d(512, 512, 4, 2),
                 nn.BatchNorm1d(512),
-                nn.ELU(),
+                nn.ELU()],
 
-                nn.Conv1d(512, self.latent_size, 4, 2),
+                [nn.Conv1d(512, self.latent_size, 4, 2),
                 nn.BatchNorm1d(self.latent_size),
-                nn.ELU()
-            )
+                nn.ELU()]
+            ]
         else:
             raise ValueError("max_seq_len must be 50 or 209 for now")
+        self.cnn = nn.Sequential(*[x for layer in self.layered_layers for x in layer])
 
     def forward(self, input):
         """
@@ -70,3 +72,18 @@ class Encoder(nn.Module):
 
         result = self.cnn(input)
         return result.squeeze(2)
+
+    def layer_out(self, input, layer=None):
+        """
+        :param input: An float tensor with shape of [batch_size, seq_len, embed_size]
+        :param layer: up to which layer to output
+        :return: An float tensor with shape of [batch_size, layer_size]
+        """
+
+        '''
+        Transpose input to the shape of [batch_size, embed_size, seq_len]
+        '''
+        input = t.transpose(input, 1, 2)
+
+        result = self.cnn[:layer](input)
+        return result.flatten(-2)
